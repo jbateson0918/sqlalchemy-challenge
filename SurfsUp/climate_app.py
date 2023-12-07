58b738f0311e5afa681b0db53822d0a84cf7619a
Jonathan Bateson <j.carr.bateson@gmail.com>
2023-12-07 13:00:12 -0500

    Start of Part 2

--- SurfsUp/climate_app.py
+++ SurfsUp/climate_app.py
@@ -1,19 +1,23 @@
 # Import the dependencies.
-
-
+from flask import Flask 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+Base = automap_base()
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
